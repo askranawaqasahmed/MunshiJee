ca550978402a8411d14ca7f6223417b4f270e7d5
ranawaqasahmed <ranawaqasahmed@outlook.com>
2026-05-06 00:44:52 +0500

    change privacy and terms page to public

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
-const PUBLIC_PATHS = new Set(["/", "/login", "/signup"]);
+const PUBLIC_PATHS = new Set(["/", "/login", "/signup", "/privacy", "/terms"]);
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({

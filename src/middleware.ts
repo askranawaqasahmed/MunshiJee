4048074ac27108ac334517da3940b9d0aa7c14ee
ranawaqasahmed <ranawaqasahmed@outlook.com>
2026-05-06 04:11:20 +0500

    Logo modification

--- src/middleware.ts
+++ src/middleware.ts
@@ -69,6 +69,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|public).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|ico|webp|avif)$).*)",
   ],
 };
